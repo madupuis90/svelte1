8991112c191d4eaebfb1ec09004d60f2479a8f67
madupuis90 <ma.dupuis.90@gmail.com>
2023-05-23 23:49:04 -0400

    progress

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { state$ } from '@app/util';
+
+</script>
+
+<style>
+  section {
+    font-size: 1.5rem;
+  }
+</style>
+
+<section>{ JSON.stringify($state$) } is mounted!</section>
